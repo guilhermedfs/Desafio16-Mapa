cd56e908f22278cd9f77e91a2aa85a7c59699888
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2022-01-12 13:47:45 -0300

    Initial Commit

---
+++ Desafio16-Mapa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Desafio16-Mapa
+//
+//  Created by Guilherme - ília on 12/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desafio16-Mapa/Desafio16_MapaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio16_MapaApp.swift
+//  Desafio16-Mapa
+//
+//  Created by Guilherme - ília on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Desafio16_MapaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
